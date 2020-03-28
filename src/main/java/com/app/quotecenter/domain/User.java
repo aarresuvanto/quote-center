b1402502c9c4bb41b8d4cd1db8dd60d733b0e8a8
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-03-28 20:49:30 +0200

    Add style to sign-up view

--- src/main/java/com/app/quotecenter/domain/User.java
+++ src/main/java/com/app/quotecenter/domain/User.java
@@ -20,30 +20,30 @@ public class User {
     private long userId;
 
     @NotNull
-    @Size(min=2, max=30)
+    @Size(min=2, max=30, message = "Must be at least 2 characters")
     @JsonIgnore
     @Column(nullable = false)
     private String firstName;
 
     @NotNull
-    @Size(min=2, max=30)
+    @Size(min=2, max=30, message = "Must be at least 2 characters")
     @JsonIgnore
     @Column(nullable = false)
     private String lastName;
 
     @NotNull
-    @Size(min=2, max=30)
+    @Size(min=2, max=30, message = "Must be at least 2 characters")
     @Column(nullable = false, unique = true)
     private String username;
 
     @NotNull
-    @Size(min=4, max=100)
+    @Size(min=4, max=100, message = "Must be at least 4 characters")
     @JsonIgnore
     @Column(name="password_hash", nullable = false)
     private String password;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Must not be blank")
+    @Email(message = "E-mail address not valid")
     @JsonIgnore
     @Column(nullable = false, unique = true)
     private String eMail;
