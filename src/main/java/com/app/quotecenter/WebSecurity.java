d9b561c853faeebd88422fb0c753a67192ab9d17
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-04-09 22:59:11 +0300

    Use local Bootstrap css file

--- src/main/java/com/app/quotecenter/WebSecurity.java
+++ src/main/java/com/app/quotecenter/WebSecurity.java
@@ -28,6 +28,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+
+        // Use only in development
+        // Enables h2-console use
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Autowired
