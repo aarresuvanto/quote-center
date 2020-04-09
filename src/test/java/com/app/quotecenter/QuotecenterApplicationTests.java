31b70f0650d5fb0d546368a4e0a7d37ca7d5a1e8
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-04-09 22:44:03 +0300

    Add controller smoke testing

--- src/test/java/com/app/quotecenter/QuotecenterApplicationTests.java
+++ src/test/java/com/app/quotecenter/QuotecenterApplicationTests.java
@@ -1,13 +1,60 @@
 package com.app.quotecenter;
 
+import com.app.quotecenter.web.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuotecenterApplicationTests {
 
+	@Autowired
+	QuoteController quoteController;
+
+	@Autowired
+	QuoteControllerRest quoteControllerRest;
+
+	@Autowired
+	QuoteListController quoteListController;
+
+	@Autowired
+	QuoteListControllerRest quoteListControllerRest;
+
+	@Autowired
+	UserController userController;
+
+	@Autowired
+	UserControllerRest userControllerRest;
+
 	@Test
-	void contextLoads() {
+	void QuoteControllerLoad() throws Exception {
+		Assertions.assertThat(quoteController).isNotNull();
+	}
+
+	@Test
+	void QuoteControllerRestLoad() throws Exception {
+		Assertions.assertThat(quoteControllerRest).isNotNull();
+	}
+
+	@Test
+	void QuoteListControllerLoad() throws Exception {
+		Assertions.assertThat(quoteListController).isNotNull();
+	}
+
+	@Test
+	void QuoteListControllerRestLoad() throws Exception {
+		Assertions.assertThat(quoteListControllerRest).isNotNull();
+	}
+
+	@Test
+	void UserControllerLoad() throws Exception {
+		Assertions.assertThat(userController).isNotNull();
+	}
+
+	@Test
+	void UserControllerRestLoad() throws Exception {
+		Assertions.assertThat(userControllerRest).isNotNull();
 	}
 
 }
